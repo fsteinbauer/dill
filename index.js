4f00bb87eaf2e762c0c68083e4fb4c49a8d9ded0
Florian Steinbauer <h50ilza@s-mxs.net>
2019-09-16 16:13:35 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+
+module.exports = () => {
+  const appRoot = process.cwd();
+
+  const cypressOptions = JSON.parse(
+    fs.readFileSync(`${appRoot}/cypress.json`, "utf-8")
+  );
+
+}
